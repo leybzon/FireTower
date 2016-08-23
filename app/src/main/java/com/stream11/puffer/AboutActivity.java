a1590b7914ac2841c1eb49a2c36448faf7b2589b
Gene <leybzon@gmail.com>
2016-08-23 08:46:02 -0700

    added blank about activity

---
+++ app/src/main/java/com/stream11/puffer/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.stream11.puffer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
